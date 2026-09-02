bf8995de77896c5478612e6f0bf707831b21d78c
Omkar Mishra <146058595+IoNiCx1@users.noreply.github.com>
2026-09-02 23:16:11 +0530

    core

--- core/asgi.py
+++ core/asgi.py
@@ -10,7 +10,9 @@ import chat.routing
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
-    'websocket': URLRouter(
-        chat.routing.websocket_urlpatterns
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     ),
 })
